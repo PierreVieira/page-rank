6c6b1022cedcd7de89d9e22678c177ff16bf854e
Nicolas Rezende <nicolasgomes0905@gmail.com>
2019-07-16 19:04:59 -0300

    adicionando calculo de page rank

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "graph.h"
+
+void set_out_degree(Graph *graph);
+void set_page_rank(Graph *graph, float tolerance);
 
 int main() {
     Graph graph;
@@ -11,10 +10,38 @@ int main() {
     get_vertices(&graph, file_name);
     create_adj_matrix(&graph, file_name);
 
-    int *out_degree = get_array_out_degree(graph);
+    // Step 1
+    set_out_degree(&graph);
+
+    //
 
     destroy_graph(graph);
+}
 
-    // show_vertices(graph);
-    // show_adj_matrix(graph);
+void set_out_degree(Graph *graph) {
+    for (int i = 0; i < graph->size; i++) {
+        int line_sum = 0;
+        for (int j = 0; j < graph->size; j++) {
+            line_sum += graph->adj_matrix[i][j];
+        }
+
+        graph->vertices[i].out_degree = line_sum;
+    }
+}
+
+void set_page_rank(Graph *graph, float tolerance) {
+    float *new_score = calloc(graph->size, sizeof(float));
+    float diff_sum;
+
+    do {
+        evaluate_current_page_rank(*graph, new_score);
+
+        diff_sum = 0;
+
+        for (int i = 0; i < graph->size; i++) {
+            diff_sum = fabs(new_score[i] - graph->vertices[i].score);
+            graph->vertices[i].score = new_score[i];
+        }
+
+    } while (diff_sum >= tolerance);
 }
